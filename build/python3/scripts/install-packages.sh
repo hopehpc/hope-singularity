11c48c61bb56d350bcc53064274d9e3441ea8e03
hopehpc <hpc@hope.edu>
2019-06-18 14:24:02 -0400

    Fix curly braces

--- build/python3/scripts/install-packages.sh
+++ build/python3/scripts/install-packages.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-packages=("$@")
-ERR=notfound.txt
+ERR=notinstalled.txt    # Error file for names of packages not installed
 
-#TODO
+# Note: The definition file runs commands at build time with /bin/sh, so it is not possible to pass a bash array; convert a comma-separated list of packages into a bash array instead
+IFS=', ' read -r -a packages <<< "$1"
+#packages=("$@")			# Array of packages passed as argument
+
 # Source try/catch functions
+source ../../scripts/trycatch.sh
 
 printf "Installing Python packages\n\n"
 
 for package in ${packages[*]}
 do
-	try {
+	try 
+	{
 		pip install $package
 	}
 
-	catch || {
+	catch || 
+	{
 		echo "Could not install pip package: $package" >> $ERR  
 	}
 
-	try {
+	try 
+	{
 		pip3 install $package
     }
 
-	catch || {
+	catch || 
+	{
 		echo "Could not install pip3 package: $package" >> $ERR
 	}
 done
