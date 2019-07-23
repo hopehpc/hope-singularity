7fbca1e33c1c706d696afac4f69edc949b0f92cf
hopehpc <hpc@hope.edu>
2019-07-23 13:32:38 -0400

    Update installation script

--- definition-files/python3/scripts/install-packages.sh
+++ definition-files/python3/scripts/install-packages.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-ERR=notinstalled.txt    # Error file for names of packages not installed
-
-# Note: The definition file runs commands at build time with /bin/sh, so it is not possible to pass a bash array; convert a comma-separated list of packages into a bash array instead
-IFS=', ' read -r -a packages <<< "$1"
-#packages=("$@")			# Array of packages passed as argument
+DATE=$(date +%Y-%m-%d)
+ERR=$DATE-notinstalled.txt    # Error file for names of packages not installed
 
 # Source try/catch functions
 source ../../scripts/trycatch.sh
 
 printf "Installing Python packages\n\n"
 
-for package in ${packages[*]}
+while read package
 do
-	try 
+    try 
 	{
 		pip install $package
 	}
@@ -32,4 +29,5 @@ do
 	{
 		echo "Could not install pip3 package: $package" >> $ERR
 	}
-done
+done < $1
+	
