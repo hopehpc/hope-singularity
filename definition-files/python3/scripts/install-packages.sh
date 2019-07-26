1c5c717ed9d91b1acd3a8b3b978495d96113da1a
hopehpc <hpc@hope.edu>
2019-07-26 11:44:23 -0400

    Update definition files

--- definition-files/python3/scripts/install-packages.sh
+++ definition-files/python3/scripts/install-packages.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 
-DATE=$(date +%Y-%m-%d)
-ERR=$DATE-notinstalled.txt    # Error file for names of packages not installed
-
-# Source try/catch functions
-source ../../scripts/trycatch.sh
-
 printf "Installing Python packages\n\n"
 
 while read package
 do
-    try 
-	{
-		pip install $package
-	}
-
-	catch || 
-	{
-		echo "Could not install pip package: $package" >> $ERR  
-	}
-
-	try 
-	{
-		pip3 install $package
-	}
-
-	catch || 
-	{
-		echo "Could not install pip3 package: $package" >> $ERR
-	}
+	pip install $package
+	pip3 install $package
 done < $1
-	
