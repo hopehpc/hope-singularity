bf8a3d5b319edd58cff34659b1bf2c9518619765
hopehpc <hpc@hope.edu>
2019-08-02 09:09:44 -0400

    Update to use /etc/profile.d to source variables

--- definition-files/build.sh
+++ definition-files/build.sh
@@ -5,11 +5,11 @@ if [ "$#" -ne 1 ]; then
 	exit 1
 fi
 
-source ../globals.sh
+source /etc/profile.d/singularity.sh
 
-DIR=${1///*}
+DIR=${1///*}					# subdirectory where def file is located
 DEF=${1#*/}					# remove path prefix of def file 
 SIF=${DEF%.def}.sif				# remove .def and add .sif extension
 
 cd $DIR						# cd in case there are files needed by the def file in the subdirectory
-sudo singularity build $SIFDIR/$SIF $DEF	# run the build command
+sudo singularity build $SIF_FILES/$SIF $DEF	# run the build command
