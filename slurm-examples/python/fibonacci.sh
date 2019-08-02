5ef714fca49ae902dac63301de0da34b0cb5d843
hopehpc <hpc@hope.edu>
2019-08-02 11:34:31 -0400

    Remove call to python3; fix variable typo

--- slurm-examples/python/fibonacci.sh
+++ slurm-examples/python/fibonacci.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=fib
 
-$SIF_FILE=${SIF_FILES}/python.sif
-$SCRIPT=scripts/fibonacci.py
+SIF_FILE=${SIF_FILES}/python.sif
+SCRIPT=scripts/fibonacci.py
 
-singularity run $SIF_FILE python3 $SCRIPT
+singularity run $SIF_FILE $SCRIPT
--- slurm-examples/python/histogram.sh
+++ slurm-examples/python/histogram.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=histogram
 
-$SIF_FILE=${SIF_FILES}/python.sif
-$SCRIPT=scripts/histogram.py
+SIF_FILE=${SIF_FILES}/python.sif
+SCRIPT=scripts/histogram.py
 
-singularity run $SIF_FILE python3 $SCRIPT
+singularity run $SIF_FILE $SCRIPT
