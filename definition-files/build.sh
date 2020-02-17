33ea37a11a148cc325932fa2958ea1065801f297
hopehpc <hpc@hope.edu>
2020-02-17 17:23:15 -0500

    changes to build.sh - BPK

--- definition-files/build.sh
+++ definition-files/build.sh
@@ -6,10 +6,13 @@ if [ "$#" -ne 1 ]; then
 fi
 
 source /etc/profile.d/singularity.sh
+export SINGULARITY_TMPDIR=/scratch
 
 DIR=${1///*}					# subdirectory where def file is located
 DEF=${1#*/}					# remove path prefix of def file 
 SIF=${DEF%.def}.sif				# remove .def and add .sif extension
 
 cd $DIR						# cd in case there are files needed by the def file in the subdirectory
-sudo singularity build $SIF_FILES/$SIF $DEF	# run the build command
+sudo --preserve-env=SINGULARITY_TMPDIR singularity build $SIF_FILES/$SIF $DEF	# run the build command
+sudo chown root:containeradmin $SIF_FILES/$SIF  # change sif file to be owned by root with group containeradmin (rather than whatever user ran the build command)
+
